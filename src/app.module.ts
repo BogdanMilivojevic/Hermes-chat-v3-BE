19c8de031e4fabe68ed79a09021fa4f9e9a0b26e
bogdan <bogdan.milivojeviczmn@gmail.com>
2023-09-28 19:15:12 +0200

    auth-test-implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,25 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from './users/user.entity';
 import { ConfigModule } from '@nestjs/config';
 
+//IMPORT CONFIG MODULE FOR ENV BEFORE EVERYTHING SO THAT ENV CAN BE USED
+
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'hermes_db',
-      models: [User],
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env`],
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database:
+        process.env.NODE_ENV === 'development'
+          ? process.env.DB_DEV
+          : process.env.DB_TEST,
+      models: [User],
     }),
     UsersModule,
     AuthModule,
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,8 +4,8 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
